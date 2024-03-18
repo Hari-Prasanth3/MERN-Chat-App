26f20f73dad32e10749691067bf8398f6c509e4c
Hari-Prasanth3 <harihari71775@email.com>
2024-03-18 12:18:04 +0530

    removed sub module

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,67 @@
+import {
+  Box,
+  Container,
+  Text,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+import React,{useEffect} from "react";
+import Login from "../components/Authentication/Login";
+import SignUp from "../components/Authentication/SignUp";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+const HomeScreen = () => {
+
+
+  const history = useHistory();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) history.push("/chats");
+  }, [history]);  
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius={"lg"}
+        borderWidth={"1px"}
+      >
+        <Text fontSize={"4xl"} bg={"whitesmoke"} fontFamily={"work sans"} textAlign={"center"}>
+          Chat App
+        </Text>
+      </Box>
+      <Box
+        w="100%"
+        p={4}
+        borderRadius={"lg"}
+        borderWidth={"1px"}
+        bg={"whitesmoke"}
+      >
+        <Tabs isLazy>
+          <TabList mb={"1em"}>
+            <Tab width={"50%"}>Login</Tab>
+            <Tab width={"50%"}>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default HomeScreen;
